dd171e2535562f69de7c0054aafb86dd1ef3dcc0
SrNexar <69290901+TheWadjet-dev@users.noreply.github.com>
2025-05-17 22:57:36 -0500

    change app

--- lib/supabase/server.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js"
-
-// Versión compatible con ambos sistemas de enrutamiento usando la clave anónima
-export const getSupabaseServer = () => {
-  return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
-}
